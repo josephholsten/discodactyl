de9df670aa690a746e70a6ef45d963b382fc6621
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2010-03-14 05:39:51 -0500

    Parse modern schema XRD links
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link::parse): Rewrite for
    	modern XRD link schema
    
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link#media_types)
    	(Discodactyl::XRD::Link#media_types=, Discodactyl::XRD::Link#uris)
    	(Discodactyl::XRD::Link#uris=, Discodactyl::XRD::Link#rels)
    	(Discodactyl::XRD::Link#rels=): Remove plural accessors
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link#href)
    	(Discodactyl::XRD::Link#href=, Discodactyl::XRD::Link#template)
    	(Discodactyl::XRD::Link#template=, Discodactyl::XRD::Link#type)
    	(Discodactyl::XRD::Link#type=, Discodactyl::XRD::Link#rel)
    	(Discodactyl::XRD::Link#rel=): Add singular accessors
    	* test/test_xrd_link_parse.rb (TestXRDLinkParsing#test_parse_media_types)
    	(TestXRDLinkParsing#test_parse_rels, TestXRDLinkParsing#test_parse_uris)
    	(TestXRDLinkParsing#test_parse_uri_templates)
    	(TestXRDLinkParsing#test_parse_all): Changed to modern sample data and
    	singular accessors
    
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link#to_uris): Removed
    	plural converter
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link#to_uri): Added
    	replacement singular converter
    	* test/test_xrd_link_parse.rb
    	(TestXRDLinkParsing#test_to_uris_for_uri_template)
    	(TestXRDLinkParsing#test_to_uris_for_plain_uri): Changed to modern sample
    	data and singular converter
    
    	* lib/discodactyl/xrd/link.rb (Discodactyl::XRD::Link::map_texts)
    	(Discodactyl::XRD::Link#has_rel?, Discodactyl::XRD::Link#has_media_type?):
    	Removed, no replacements provided
    
    	* test/test_xrd_link_parse.rb (TestXRDLinkParsing#assert_has_media_type?)
    	(TestXRDLinkParsing#test_has_rel?, TestXRDLinkParsing#assert_has_rel?)
    	(TestXRDLinkParsing#test_to_uris_for_mixed_uris_and_uri_templates)
    	(TestXRDLinkParsing#test_has_media_type?): Removed

--- lib/discodactyl/xrd/link.rb
+++ lib/discodactyl/xrd/link.rb
@@ -1,34 +1,22 @@
 require 'nokogiri'
+require 'active_support/core_ext/object/misc'
 
 module Discodactyl
 module XRD
   class Link
     class << self
       def parse(element)
-        link = self.new
-        namespaces = {'xrd' => "http://docs.oasis-open.org/ns/xri/xrd-1.0"}
-
-        link.rels =  map_texts(element, 'xrd:Rel', namespaces)
-        link.media_types =  map_texts(element, 'xrd:MediaType', namespaces)
-        link.uris =  map_texts(element, 'xrd:URI', namespaces)
-        link.uris = link.uris + element.xpath('xrd:URITemplate', namespaces).map {|elem|
-          URITemplate.new elem.text
-        }
-        link
-      end
-      def map_texts(element, path, namespaces)
-        element.xpath(path, namespaces).map {|elem| elem.text }
+        returning(link = self.new) do
+          link.rel = element['rel']
+          link.type = element['type']
+          link.href = element['href']
+          link.template = URITemplate.new(element['template']) unless link.href
+        end
       end
     end
-    attr_accessor :uris, :rels, :media_types
-    def has_rel?(rel)
-      @rels.include? rel
-    end
-    def has_media_type?(media_type)
-      @media_types.include? media_type
-    end
-    def to_uris(params = {})
-      @uris.collect{|u| u.respond_to?('to_uri') ? u.to_uri(params) : u }
+    attr_accessor :href, :template, :rel, :type
+    def to_uri(params = {})
+      @href || @template.to_uri(params)
     end
   end
 end
--- test/test_xrd_link_parse.rb
+++ test/test_xrd_link_parse.rb
@@ -13,19 +13,15 @@ class TestXRDLinkParsing < Test::Unit::TestCase
     @namespaces = {'xrd' => "http://docs.oasis-open.org/ns/xri/xrd-1.0"}
   end
   def test_parse_all
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><MediaType>text/html</MediaType><Rel>http://webfinger.info/rel/service</Rel><URI>http://host.example</URI><URITemplate>http://www.google.com/s2/webfinger/?q={%id}</URITemplate></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link rel="lrdd" type="text/html" href="http://host.example" template="http://www.google.com/s2/webfinger/?q={%id}"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
 
     link = Discodactyl::XRD::Link.parse(elem)
 
-    assert_not_nil(link)
-    assert_length(1, link.rels)
-    assert_include?('http://webfinger.info/rel/service', link.rels)
-    assert_length(1, link.media_types)
-    assert_include?('text/html', link.media_types)
-    assert_length(2, link.uris)
-    assert_include?('http://host.example', link.uris)
-    assert_include?(URITemplate.new('http://www.google.com/s2/webfinger/?q={%id}'), link.uris)
+    assert_equal 'lrdd', link.rel
+    assert_equal 'text/html', link.type
+    assert_equal 'http://host.example', link.href
+    assert_nil link.template
   end
 
   def test_parse_empty
@@ -38,102 +34,58 @@ class TestXRDLinkParsing < Test::Unit::TestCase
   end
 
   def test_parse_rels
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><Rel>http://webfinger.info/rel/service</Rel></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link rel="lrdd"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
 
     link = Discodactyl::XRD::Link.parse(elem)
 
-    assert_length(1, link.rels)
-    assert_include?('http://webfinger.info/rel/service', link.rels)
+    assert_equal 'lrdd', link.rel
   end
 
   def test_parse_media_types
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><MediaType>text/html</MediaType></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link type="text/html"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
 
     link = Discodactyl::XRD::Link.parse(elem)
 
-    assert_length(1, link.media_types)
-    assert_include?('text/html', link.media_types)
+    assert_equal 'text/html', link.type
   end
 
   def test_parse_uris
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><URI>http://host.example</URI></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link href="http://host.example"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
 
     link = Discodactyl::XRD::Link.parse(elem)
 
-    assert_length(1, link.uris)
-    assert_include?('http://host.example', link.uris)
+    assert_equal 'http://host.example', link.href
   end
 
   def test_parse_uri_templates
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><URITemplate>http://www.google.com/s2/webfinger/?q={%id}</URITemplate></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link template="http://www.google.com/s2/webfinger/?q={%id}"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
 
     link = Discodactyl::XRD::Link.parse(elem)
 
-    assert_length(1, link.uris)
-    assert_include?(URITemplate.new('http://www.google.com/s2/webfinger/?q={%id}'), link.uris)
-  end
-
-  def test_to_uris_for_mixed_uris_and_uri_templates
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><URI>http://host.example</URI><URITemplate>http://www.google.com/s2/webfinger/?q={%id}</URITemplate></Link></XRD>'
-    elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
-    link = Discodactyl::XRD::Link.parse(elem)
-
-    uris = link.to_uris 'id' => 'bob@gmail.com'
-
-    assert_length(2, uris)
-    assert_include?('http://host.example', uris)
-    assert_include?('http://www.google.com/s2/webfinger/?q=bob@gmail.com', uris)
+    assert_equal URITemplate.new('http://www.google.com/s2/webfinger/?q={%id}'), link.template
   end
 
   def test_to_uris_for_plain_uri
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><URI>http://host.example</URI></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link href="http://host.example"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
     link = Discodactyl::XRD::Link.parse(elem)
 
-    uris = link.to_uris
+    uri = link.to_uri
 
-    assert_length(1, uris)
-    assert_include?('http://host.example', uris)
+    assert_equal 'http://host.example', uri
   end
 
   def test_to_uris_for_uri_template
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><URITemplate>http://www.google.com/s2/webfinger/?q={%id}</URITemplate></Link></XRD>'
+    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link template="http://www.google.com/s2/webfinger/?q={%id}"/></XRD>'
     elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
     link = Discodactyl::XRD::Link.parse(elem)
 
-    uris = link.to_uris 'id' => 'bob@gmail.com'
+    uri = link.to_uri 'id' => 'bob@gmail.com'
 
-    assert_length(1, uris)
-    assert_include?('http://www.google.com/s2/webfinger/?q=bob@gmail.com', uris)
-  end
-
-  def test_has_media_type?
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><MediaType>text/html</MediaType></Link></XRD>'
-    elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
-    link = Discodactyl::XRD::Link.parse(elem)
-
-    assert_has_media_type?(link, 'text/html')
-  end
-
-  def test_has_rel?
-    raw = '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0"><Link><Rel>http://webfinger.info/rel/service</Rel><URITemplate>http://www.google.com/s2/webfinger/?q={%id}</URITemplate></Link></XRD>'
-    elem = Nokogiri(raw).xpath('/xrd:XRD/xrd:Link', @namespaces).first
-    link = Discodactyl::XRD::Link.parse(elem)
-
-    assert_has_rel?(link, 'http://webfinger.info/rel/service')
-  end
-
-  def assert_has_media_type?(link, media_type, message = nil)
-    message = build_message message, '<?> does not have the media_type <?>.', link, media_type
-    assert link.has_media_type?(media_type), message
-  end
-
-  def assert_has_rel?(link, rel, message = nil)
-    message = build_message message, '<?> does not have the rel <?>.', link, rel
-    assert link.has_rel?(rel), message
+    assert_equal 'http://www.google.com/s2/webfinger/?q=bob@gmail.com', uri
   end
 end
