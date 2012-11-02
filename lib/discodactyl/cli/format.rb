da98acb9794f224ac05ccec023cf5243c1ceec5b
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2012-11-02 00:08:38 -0700

    extract formatting into a unified interface and out of the main script

---
+++ lib/discodactyl/cli/format.rb
@@ -0,0 +1,55 @@
+module Discodactyl # :nodoc
+module CLI # :nodoc
+# Formatting helpers for various rels
+class Format
+  def self.for_rel(rel)
+    case rel
+    when 'http://microformats.org/profile/hcard' then
+      lambda {|uri, io| Discodactyl::CLI::Format.hcard_from_uri(uri, io) }
+    when 'http://schemas.google.com/g/2010#updates-from' then
+      lambda {|uri, io| Discodactyl::CLI::Format.activities_from_uri(uri, io) }
+    else
+      name = Discodactyl::KNOWN_RELS[rel]
+      lambda {|uri, io| io.puts "#{name}: #{uri}" if uri }
+    end
+  end
+
+  def self.hcard_adr(adr)
+    segments = []
+    segments << adr.street_address if adr.properties.include?(:street_address)
+    segments << adr.locality if adr.properties.include?(:locality)
+    segments << adr.region if adr.properties.include?(:region)
+    segments << adr.postal_code if adr.properties.include?(:postal_code)
+    segments << adr.country_name if adr.properties.include?(:country_name)
+    segments.join(' ')
+  end
+
+  def self.hcard_from_uri(uri, io)
+    begin
+      hcards = Prism.find(uri, :hcard)
+      hcards.each do |hcard|
+        if hcard
+          io << "Name: #{hcard.fn.gsub(/\s+/, ' ')}\n" if hcard.properties.include?(:fn)
+          io << "Title: #{hcard.title}\n" if hcard.properties.include?(:title)
+          io << "Organization: #{hcard.org}\n" if hcard.properties.include?(:org)
+          io << "Address: #{format_hcard_adr(hcard.adr)}\n" if hcard.properties.include?(:adr)
+        end
+      end
+    rescue RuntimeError => e
+      Discodactyl.log.warn(e.message)
+      Discodactyl.log.debug(e)
+    end
+  end
+
+  def self.activities_from_uri(uri, io)
+    if uri && feed = Feedzirra::Feed.fetch_and_parse(uri)&& entry = feed.entries.first
+      include ActionView::Helpers::DateHelper
+      silence_warnings do
+        io.puts "Status: #{Loofah::Helpers.strip_tags(entry.content)} #{time_ago_in_words(entry.published)} ago"
+      end
+    end
+  end
+
+end
+end
+end
--- lib/discodactyl/known_rels.rb
+++ lib/discodactyl/known_rels.rb
@@ -1,12 +1,14 @@
 module Discodactyl # :nodoc:
   KNOWN_RELS = {
-    'OpenID' => 'http://specs.openid.net/auth/',
-    'OpenID Provider' => 'http://specs.openid.net/auth/2.0/provider',
-    'OpenID Provider' => 'openid2.provider',
-    "Profile" => 'http://webfinger.net/rel/profile-page',
-    "Profile data" => 'http://portablecontacts.net/spec/1.0#me',
-    "Contacts" => 'http://portablecontacts.net/spec/1.0',
-    "describedby" => 'describedby', # from POWDER
-    "Webfinger/LRDD" => 'lrdd'
+    'http://microformats.org/profile/hcard' => 'hcard',
+    'http://specs.openid.net/auth/' => 'OpenID',
+    'http://specs.openid.net/auth/2.0/provider' => 'OpenID Provider',
+    'openid2.provider' => 'OpenID Provider',
+    'http://webfinger.net/rel/profile-page' => 'Profile',
+    'http://portablecontacts.net/spec/1.0#me' => 'Profile data',
+    'http://portablecontacts.net/spec/1.0' => 'Contacts',
+    'describedby' => 'describedby', # from POWDER
+    'lrdd' => 'Webfinger/LRDD',
+    'http://schemas.google.com/g/2010#updates-from' => 'activities'
   }
 end
